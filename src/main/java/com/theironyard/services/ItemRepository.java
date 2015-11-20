320957dbd57ff4ab6e441b52e9cdaa5564b9b713
holdenhughes <holdenmh121489@gmail.com>
2015-11-20 13:32:22 -0500

    Built jar file

--- src/main/java/com/theironyard/services/ItemRepository.java
+++ src/main/java/com/theironyard/services/ItemRepository.java
@@ -1,9 +1,10 @@
 package com.theironyard.services;
 
+import com.theironyard.entities.Item;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by benjamindrake on 11/19/15.
  */
-public interface ItemRepository extends CrudRepository {
+public interface ItemRepository extends CrudRepository<Item, Integer> {
 }
